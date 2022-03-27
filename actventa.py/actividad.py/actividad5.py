27367a885ae4a630474a2a8de9ab4377514ad9d4
Damian Puerta <101460318+GissetPuerta@users.noreply.github.com>
2022-03-27 15:21:38 -0500

    h
    h

---
+++ actventa.py/actividad.py/actividad5.py
@@ -0,0 +1,7 @@
+x1=int(input("digite el valor de X1: "))
+y1=int(input("digite el valor de Y1: "))
+x2=int(input("digite el valor de X2: "))
+y2=int(input("digite el valor de Y2: "))
+
+d=((x2-x1)**2+(y2-y1)**2)**(1/2)
+print("la distancia euclidea entre dos puntos es: ",d)
---
+++ actventa.py/ejercicio2.py
@@ -0,0 +1,5 @@
+e=int(input("ingrese la cantidad de estudiantes: "))
+p=int(input("ingrese la cantidad de estudiantes que perdieron: "))
+
+x=(p*100)/e
+print("el porcentaje de estudiantes que perdieron fue de: ",x)
